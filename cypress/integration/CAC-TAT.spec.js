5ae17d2bd1689b427a9b0fabb8a9b3d7f10e028e
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2023-05-13 20:34:52 -0300

    resolvido: testes no formulario de cadastro

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,24 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach( () => {
         cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
-  })
+
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+        cy.get('#firstName').type('William')
+        cy.get('#lastName').type('Almeida')
+        cy.get('#email').type('william@gmail.com')
+        cy.get('#open-text-area').type('Alguma coisa muito legal!')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
+    })
+
+    
+})
   
